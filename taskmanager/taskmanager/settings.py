7eb5c4d91ead301d1841962db909c5b7ed88b219
Cukierrrrr <rafalfilipczak34@gmail.com>
2025-12-10 10:29:19 +0100

    poprwaki

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -76,10 +76,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "tasks",
-        "USER": "admin",
-        "PASSWORD": "haslo123",
+        "USER": DB_USER,
+        "PASSWORD":  DB_PASSWORD,
         "HOST": DB_HOST,
-        "PORT": "5432",
+        "PORT": DB_PORT,
     }
 }
 
